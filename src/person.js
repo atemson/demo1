c8a135cb955060cac420ff59e3cdc127bbc33892
Atemson <mbengatemson@yahoo.com>
2022-08-10 14:29:45 +0800

    Initial commit

---
+++ src/person.js
@@ -0,0 +1,7 @@
+function displayPerson(name, age, height) {
+    let str1 = "Person named " + name + ",  " + age + " years old";
+    let str2 = (height !== undefined) ? (" and " + height +  " feet tall") : '';
+    console.log(str1 + str2);
+}
+
+displayPerson( "John", 32);
